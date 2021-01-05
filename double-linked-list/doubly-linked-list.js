836d03856a2b81c045d8c65b11b18a30da884ea4
Ivan Mitkov <ivan.mitkov@gmail.com>
2021-01-05 17:25:42 +0200

    doubly linked list

--- double-linked-list/doubly-linked-list.js
+++ double-linked-list/doubly-linked-list.js
@@ -75,25 +75,103 @@ export class DoublyLinkedList {
     this.length--;
     return first;
   }
-  unshift(val){
-     //create new node
-     let newNode = new Node(val);
-     //if list is empty
-     if (this.head === null) {
-       this.head = newNode;
-       this.tail = newNode;
-       this.length++;
-       return this;
-     }
-     //this.head is the old first
-     //it's prev is now the new first
-     this.head.prev = newNode;
-     //next of the new first is the old first
-     newNode.next = this.head;
-     //the new head is the new node
-     this.head = newNode;
-     this.length++;
-     return this;
+  unshift(val) {
+    //create new node
+    let newNode = new Node(val);
+    //if list is empty
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+      this.length++;
+      return this;
+    }
+    //this.head is the old first
+    //it's prev is now the new first
+    this.head.prev = newNode;
+    //next of the new first is the old first
+    newNode.next = this.head;
+    //the new head is the new node
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+  get(position) {
+    if (position < 0 || position >= this.length) {
+      return null;
+    }
+    if (position <= this.length / 2) {
+      let current = this.head;
+      let counter = 0;
+      while (counter < position) {
+        current = current.next;
+        counter++;
+      }
+      return current;
+    }
+    console.log("From tail");
+    let current = this.tail;
+    let counter = this.length - 1;
+    while (counter < position) {
+      current = current.prev;
+      counter--;
+    }
+    return current;
+  }
+  set(value, position) {
+    let current = this.get(position);
+    if (current !== null) {
+      current.val = value;
+      return current;
+    }
+    return false;
+  }
+
+  insert(value, position) {
+    if (position < 0 || position > this.length) {
+      return false;
+    }
+    if (position === 0) {
+      return !!this.unshift(value);
+    }
+    if (position === this.length) {
+      return !!this.push(value);
+    }
+    const newNode = new Node(value);
+    //find element before position
+    const prev = this.get(position - 1);
+    let afterNode = prev.next;
+    //connect element before the position with new node
+    prev.next = newNode;
+    newNode.prev = prev;
+    //connect inserted node with element after position
+    newNode.next = afterNode;
+    afterNode.prev = newNode;
+
+    this.length++;
+    return true;
+  }
+  remove(position) {
+    if (position < 0 || position >= this.length) {
+      return false;
+    }
+    if (position === 0) {
+      return this.shift();
+    }
+    if (position === this.length - 1) {
+      return this.pop();
+    }
+
+    let toBeRemoved = this.get(position - 1);
+    let previos = toBeRemoved.prev;
+    let nextNode = toBeRemoved.next;
+    //connect  node before removed with element after removed
+    previos.next = nextNode;
+    nextNode.prev = previos;
+    //clear links in removed
+    toBeRemoved.next = null;
+    toBeRemoved.prev = null;
+    this.length--;
+    return toBeRemoved;
   }
 
   print() {
--- double-linked-list/doublyCheck.js
+++ double-linked-list/doublyCheck.js
@@ -1,31 +1,76 @@
 import { DoublyLinkedList } from "./doubly-linked-list.js";
 
 const list = new DoublyLinkedList();
-list.push(10);
-list.push(11);
-list.push("Last Item");
-list.print();
+// list.push(10);
+// list.push(11);
+// list.push("Last Item");
+// list.print();
+// // console.log(list);
+// const last = list.pop();
+// console.log(last);
+// list.print();
+// console.log("SHIFT");
+// const first = list.shift();
+// // console.log(first);
+// list.print();
+// // console.log(list);
+// list.push(32);
+// list.print();
+// // console.log(list);
+
+// list.shift();
+// list.print();
+// list.shift();
+
+// // console.log(list);
+// console.log("UNSHIFT");
+// list.unshift(1);
+// list.unshift(2);
+// list.unshift(3);
+// list.print();
 // console.log(list);
-const last = list.pop();
-console.log(last);
+// console.log("GET");
+// console.log(list.get(0));
+// console.log(list.get(1));
+// console.log(list.get(2));
+// // console.log(list.get(3));
+// list.print();
+// console.log("SET");
+
+// console.log(list.set("Hi", 0));
+// console.log(list.set("There", 1));
+// console.log(list.set("again", 2));
+// list.print();
+// // console.log(list);
+
+// console.log("INSERT");
+// list.pop();
+// list.pop();
+// list.pop();
+list.insert("0", 0);
+// list.print();
+
+list.insert(1, 0);
+list.insert(2, 1);
+
+list.insert(6, list.length);
 list.print();
-console.log("SHIFT");
-const first = list.shift();
-// console.log(first);
-list.print();
+console.log(list);
+
 // console.log(list);
-list.push(32);
-list.print();
+console.log("REMOVE");
+// list.print();
+// list.remove(2);
+// list.print();
 // console.log(list);
 
-list.shift();
 list.print();
-list.shift();
 
-// console.log(list);
-console.log("UNSHIFT")
-list.unshift(1)
-list.unshift(2)
-list.unshift(3)
-list.print()
-console.log(list)
+list.remove(0);
+list.print();
+console.log(list);
+list.remove(list.length - 1);
+list.print();
+let node = list.remove(1);
+console.log(node);
+list.print();
